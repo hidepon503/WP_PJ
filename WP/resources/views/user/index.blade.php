577eb5bb31d81da3d2b23766aaecd60bb49d1896
hidepon503 <blue.passion625@gmail.com>
2023-10-02 22:03:04 +0900

    フロント調整

--- WP/resources/views/user/index.blade.php
+++ WP/resources/views/user/index.blade.php
@@ -66,27 +66,37 @@
             <h2>新着登録猫一覧</h2>
             <div class="flex flex-wrap">
                 @foreach($cats as $cat)
-                    <div class="catcontainer w-full md:w-1/4 py-5 md:px-5">
-                        <div class=" px-2 bg-white shadow rounded h-56 py-6 {{ $cat->status_id == 4 ? 'blue-corner' : '' }}">
-                            <div class="flex flex-col justify-center items-center  mb-4">
-                                <!-- 仮定として、CatImageモデルと関連付けがされており、最初の画像を取得できるとします -->
-                                <a href="{{ route('cat.show', $cat->id) }}" class="">
-                                    <img class="h-24 w-24 mb-2 rounded-full object-cover" src="{{ asset('storage/images/cats/' . $cat->image) }}" alt="{{ $cat->name }}">
-                                </a>
-                                    <div class="">
-                                        <p class="text-l text-center">{{ $cat->name }}</p>
-                                        <!-- 仮定として、genderとkindの関係も設定されているとします -->
-                                        <p class="text-blueGray-400 text-center text-xs">{{ $cat->kind->kind }}</p>
-                                        <p class="text-xs text-center text-blueGray-400">{{ $cat->age }}歳  ({{ $cat->gender->gender }})</p>
-                                        @livewire('favorite-component', ['catId' => $cat->id])
-                                    </div>
-                                </div>
-                                <p class="leading-loose text-blueGray-400 mb-5 whitespace-pre-line">
-                                    <!-- 何かのテキスト情報を表示したい場合、以下のようにすることができます -->
-                                    {{ $cat->description }} 
+                    <div class="catcontainer w-full md:w-1/4 py-3  md:px-3">
+                    <!-- status_idに基づいてクラスを動的に追加 -->
+                    <div class=" bg-white shadow rounded  py-6 {{ $cat->status_id == 4 ? 'blue-corner' : '' }}">
+                        <div class="px-2 flex flex-col justify-center items-center ">
+                            <!-- 画像、名前、性別、種類、お気に入りボタンなどの表示部分はそのまま保持 -->
+                            <a href="{{ route('cat.show', $cat->id) }}" class="">
+                                <img class="h-24 w-24 mb-2 rounded-full object-cover" src="{{ asset('storage/images/cats/' . $cat->image) }}" alt="{{ $cat->name }}">
+                            </a>
+                            <div class="" style="">
+                                <p class="text-l mb-1 text-center" style="font-size:14px;">{{ $cat->name }}
+                                    <span class="" style="font-size:8px;">
+                                        {{ $cat->age }}歳  {{ $cat->gender->gender }}
+                                    </span>
                                 </p>
+                                <p class="text-blueGray-400 text-center mb-2" style="font-size: 8px;">{{ $cat->kind->kind }} </p>
+                                <div class="flex justify-end items-center" style="padding-right:-2px;">
+                                    <span class="mr-2" style="font-size:8px;">
+                                        {{ $cat->admin->prefecture }}
+                                    </span>
+                                    <span class="">
+                                        @livewire('favorite-component', ['catId' => $cat->id])
+                                    </span>
+                                </div>
+                                
                             </div>
+                        </div>
+                        
+                        <!-- status_idが3か4のときに"家族決定"のテキストを表示 -->
+
                     </div>
+                </div>
                 @endforeach
             </div>
         </div>
